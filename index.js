68771d476fb6b4ee54ade2a0a5b61fa83f24fb06
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-05-30 00:35:28 +0100

    Refactor computerPlay to computerSelection

--- index.js
+++ index.js
@@ -3,35 +3,37 @@ const playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase()
 const computerPlay = () => {
   const arr = ["rock", "paper", "scissors"]
   return arr[Math.floor(Math.random() * 3)]
-} 
+}
 
-console.log(playRound(playerSelection, computerPlay()))
+const computerSelection = computerPlay()
 
-function playRound(playerSelection, computerPlay) {
+console.log(playRound(playerSelection, computerSelection))
+
+function playRound(playerSelection, computerSelection) {
   switch (playerSelection){
     case "rock":
-      return checker(playerSelection, computerPlay, ["rock","paper","scissors"]);
+      return checker(playerSelection, computerSelection, ["rock","paper","scissors"]);
       break;
     case "paper":
-      return checker(playerSelection, computerPlay, ["paper","scissors","rock"])
+      return checker(playerSelection, computerSelection, ["paper","scissors","rock"])
       break;
     case "scissors":
-      return checker(playerSelection, computerPlay, ["scissors","rock","paper"])
+      return checker(playerSelection, computerSelection, ["scissors","rock","paper"])
       break;
     default:
       return "You clearly do not want to play"
   }
 }
 
-function checker(playerSelection, computerPlay, arr) {
-  switch (arr.indexOf(computerPlay)) {
+function checker(playerSelection, computerSelection, arr) {
+  switch (arr.indexOf(computerSelection)) {
     case 0:
       return `You drew`;
       break;
     case 1:
-      return `You lose, ${computerPlay} beats ${playerSelection}`;
+      return `You lose, ${computerSelection} beats ${playerSelection}`;
       break;
     default:
-      return `You won, ${playerSelection} beats ${computerPlay}`
+      return `You won, ${playerSelection} beats ${computerSelection}`
   }
 }
